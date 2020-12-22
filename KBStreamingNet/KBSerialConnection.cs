a135df2f2f78e65c5ae80bf25c60ebe6d42bc88b
nazabk <krzysztof.bazan@hub.pl>
2020-12-22 14:29:42 +0100

    Add KBStreamingNet project

---
+++ KBStreamingNet/KBSerialConnection.cs
@@ -0,0 +1,131 @@
+﻿using KBCommunication.Interfaces;
+using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KBStreamingNet
+{
+    public class KBSerialConnection : IConnection<byte[]>
+    {
+        private readonly SemaphoreSlim sendSemaphore = new SemaphoreSlim(1, 1);
+        private readonly SerialPort serialPort;
+
+        private bool active;
+
+        public KBSerialConnection(string portName, int baudRate = 115200, Parity parity = Parity.None, int dataBits = 8, StopBits stopBits = StopBits.One)
+        {
+            serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.ErrorReceived += SerialPort_ErrorReceived;
+        }
+
+        public event EventHandler<byte[]> Received;
+
+        public static int ReconnectionDelay { get; set; } = 500;
+
+        public bool Active
+        {
+            get => serialPort.IsOpen;
+            set
+            {
+                if (active == value) return;
+                active = value;
+                if (value)
+                {
+                    serialPort.Open();
+                }
+                else
+                {
+                    serialPort.Close();
+                }
+            }
+        }
+
+        public async Task Send(byte[] data, CancellationToken cancelToken)
+        {
+            await sendSemaphore.WaitAsync(cancelToken);
+            try
+            {
+                if (data.Length > 0)
+                {
+                    await serialPort.BaseStream.WriteAsync(data, 0, data.Length, cancelToken);
+                }
+            }
+            finally
+            {
+                sendSemaphore.Release();
+            }
+        }
+
+        #region communication
+
+        private void SerialPort_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            if (sender is SerialPort port)
+            {
+                port.Close();
+                Thread.Sleep(ReconnectionDelay);
+                if (active)
+                {
+                    port.Open();
+                }
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                var port = sender as SerialPort;
+                var bytesReceived = port.BytesToRead;
+                if (bytesReceived > 0)
+                {
+                    var buffer = new byte[bytesReceived];
+                    port.Read(buffer, 0, buffer.Length);
+                    Task.Run(delegate { Received?.Invoke(this, buffer); });
+                }
+            }
+            catch (Exception ex) when (
+            ex is InvalidOperationException ||
+            ex is TimeoutException)
+            { }
+        }
+
+        #endregion communication
+
+        #region IDispose
+
+        // To detect redundant calls
+        private bool _disposed = false;
+
+        /// <summary>
+        /// Disponses class.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        /// <summary>
+        /// Disposes class.
+        /// </summary>
+        /// <param name="disposing">Flag if managed resources should by disposed.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Active = false;
+            }
+
+            _disposed = true;
+        }
+
+        #endregion IDispose
+    }
+}
---
+++ KBStreamingNet/KBTcpConnection.cs
@@ -0,0 +1,228 @@
+﻿using KBCommunication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KBStreamingNet
+{
+    public class KBTcpConnection : IConnection<byte[]>
+    {
+        private readonly IPAddress address;
+        private readonly int port;
+        private readonly byte[] buffer;
+        private readonly SemaphoreSlim sendSemaphore = new SemaphoreSlim(1, 1);
+
+        private TcpClient tcpClient;
+        private bool active;
+
+        public KBTcpConnection(IPAddress address, int port)
+        {
+            this.address = address;
+            this.port = port;
+            buffer = new byte[BufferSize];
+        }
+
+        public event EventHandler<byte[]> Received;
+
+        public static int BufferSize { get; set; } = 102400;
+
+        public static int ReconnectionDelay { get; set; } = 100;
+
+        public bool IsConnected
+        {
+            get
+            {
+                try
+                {
+                    if (tcpClient != null && tcpClient.Client != null && tcpClient.Client.Connected)
+                    {
+                        /* pear to the documentation on Poll:
+                         * When passing SelectMode.SelectRead as a parameter to the Poll method it will return
+                         * -either- true if Socket.Listen(Int32) has been called and a connection is pending;
+                         * -or- true if data is available for reading;
+                         * -or- true if the connection has been closed, reset, or terminated;
+                         * otherwise, returns false
+                         */
+
+                        // Detect if client disconnected
+                        if (tcpClient.Client.Poll(0, SelectMode.SelectRead))
+                        {
+                            byte[] buff = new byte[1];
+                            if (tcpClient.Client.Receive(buff, SocketFlags.Peek) == 0)
+                            {
+                                // Client disconnected
+                                return false;
+                            }
+                            else
+                            {
+                                return true;
+                            }
+                        }
+
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool Active
+        {
+            get => active;
+            set
+            {
+                if (active == value) return;
+                active = value;
+                if (active)
+                {
+                    Connect();
+                }
+                else
+                {
+                    tcpClient?.Close();
+                }
+            }
+        }
+
+        public async Task Send(byte[] data, CancellationToken cancelToken)
+        {
+            await sendSemaphore.WaitAsync(cancelToken);
+            try
+            {
+                if (data.Length > 0)
+                {
+                    await tcpClient.GetStream().WriteAsync(data, 0, data.Length, cancelToken);
+                }
+            }
+            finally
+            {
+                sendSemaphore.Release();
+            }
+        }
+
+        #region communication
+
+        private void Connect()
+        {
+            while (active)
+            {
+                try
+                {
+                    tcpClient = new TcpClient();
+                    tcpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+                    tcpClient.BeginConnect(address, port, OnConnected, tcpClient);
+                    return;
+                }
+                catch (Exception ex) when (ex is ObjectDisposedException || ex is SocketException)
+                {
+                    Reconnect(tcpClient);
+                }
+            }
+        }
+
+        private void OnConnected(IAsyncResult ar)
+        {
+            var client = ar.AsyncState as TcpClient;
+            try
+            {
+                client.EndConnect(ar);
+                ReadMessage(client);
+            }
+            catch (Exception ex) when (ex is ObjectDisposedException || ex is SocketException)
+            {
+                Reconnect(client);
+            }
+        }
+
+        private void ReadMessage(TcpClient client)
+        {
+            if (!active) return;
+            try
+            {
+                client.GetStream().BeginRead(buffer, 0, buffer.Length, OnReadMessge, client);
+            }
+            catch (Exception ex) when (
+            ex is ObjectDisposedException ||
+            ex is IOException ||
+            ex is InvalidOperationException)
+            {
+                Reconnect(client);
+            }
+        }
+
+        private void OnReadMessge(IAsyncResult ar)
+        {
+            var client = ar.AsyncState as TcpClient;
+            try
+            {
+                var readed = client.GetStream().EndRead(ar);
+                if (readed == 0) throw new IOException();
+                var data = buffer.Take(readed).ToArray();
+                Task.Run(delegate { Received?.Invoke(this, data); });
+                ReadMessage(client);
+            }
+            catch (Exception ex) when (
+            ex is ObjectDisposedException ||
+            ex is IOException ||
+            ex is InvalidOperationException)
+            {
+                Reconnect(client);
+            }
+        }
+
+        private async void Reconnect(TcpClient client)
+        {
+            client?.Close();
+            await Task.Delay(ReconnectionDelay);
+            Connect();
+        }
+
+        #endregion communication
+
+        #region IDispose
+
+        // To detect redundant calls
+        private bool _disposed = false;
+
+        /// <summary>
+        /// Disponses class.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        /// <summary>
+        /// Disposes class.
+        /// </summary>
+        /// <param name="disposing">Flag if managed resources should by disposed.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Active = false;
+            }
+
+            _disposed = true;
+        }
+
+        #endregion IDispose
+    }
+}
